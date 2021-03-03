26575b2d92d257b1aa900415f9ccc6ed8eca5474
kicandray <kevin.candray.688@my.csun.edu>
2021-03-03 10:54:14 -0800

    adding week 6 folder

--- Week_5/Scripts/Week5B_LabAssignment.R
+++ Week_5/Scripts/Week5B_LabAssignment.R
@@ -17,13 +17,33 @@ View(DepthData)
 ConData_sqkey <- CondData %>% 
   mutate(DateTime = ymd_hms(date)) %>% 
   mutate(DateTime + seconds(8)) %>% 
-  rename(
-    "old date" = "date",
-    "date" = "DateTime + seconds(8)") %>% 
-  select()
+  rename(date_old = date) %>% 
+  rename(na_date = DateTime) %>% 
+  rename(date = "DateTime + seconds(8)") %>% 
+  select(-date_old, -na_date)
 FullData <- inner_join(ConData_sqkey, DepthData)
 View(FullData)
-Fulldata %>% 
+FullData <- FullData %>% 
   mutate(time_hour = hour(date)) %>%
-  mutate(time_minute = minute(date))
+  mutate(time_minute = minute(date)) %>% 
+  group_by(time_hour, time_minute) %>% 
+  summarise(mean_date = mean(date), 
+            mean_depth = mean(Depth),  
+            mean_temperature = mean(TempInSitu),  
+            mean_salinity = mean(SalinityInSitu_1pCal))%>%
+  write_csv(here("Week_5","Data","FullData.csv"))
 
+#########################################
+#plot
+FullData %>% # name the data frame 
+  ggplot(aes(x = mean_temperature, # name x axis 
+             y = mean_depth, # name y axis 
+             color = mean_salinity))+
+  geom_line()+
+  labs(title = "Average Temperature, Depth and Salinity",
+       x= "Average Temp (Celcius)", 
+       y= "Average Depth (Meters)")+
+  theme_classic()+
+  theme(axis.title = element_text(size = 12))
+  ggsave(here("Week_5","Output","AvgTempDepthLab.png"),
+         width = 7, height = 5)
---
+++ Week_6/Scripts/Week6A_Lecture.R
@@ -0,0 +1,11 @@
+#What is RMarkdown 
+#combining data and text together, used a lot but data scientist 
+#document format (.Rmd)
+#r package 
+#wizardry :)
+#WHATS IN IT 
+  #METADATA: YAML
+    #KEY: VALUE
+  #TEXT
+  #cODE
+  #OUTPUT
